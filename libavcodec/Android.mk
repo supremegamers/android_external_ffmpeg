58d7a4a8612a82371f16ccabddab16edcaf55488
hmtheboy154 <buingoc67@gmail.com>
2023-05-13 12:30:43 -0400

    Enable AV1 decoder using dav1d or VAAPI
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- libavcodec/Android.mk
+++ libavcodec/Android.mk
@@ -23,6 +23,10 @@ ifeq ($(CONFIG_VAAPI),yes)
   LOCAL_SHARED_LIBRARIES += libva
 endif
 
+ifeq ($(CONFIG_LIBDAV1D),yes)
+  LOCAL_SHARED_LIBRARIES += libdav1d
+endif
+
 ifneq ($(ARCH_ARM_HAVE_NEON),)
   LOCAL_SRC_FILES += neon/mpegvideo.c
 endif
@@ -41,6 +45,10 @@ LOCAL_SHARED_LIBRARIES +=	\
 
 ifeq ($(CONFIG_VAAPI),yes)
   LOCAL_SHARED_LIBRARIES += libva
+endif
+
+ifeq ($(CONFIG_LIBDAV1D),yes)
+  LOCAL_SHARED_LIBRARIES += libdav1d
 endif
 
 ifneq ($(ARCH_ARM_HAVE_NEON),)
